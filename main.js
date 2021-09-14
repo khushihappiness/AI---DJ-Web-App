d699cf126c2ac774dfb9c8db493d4039e4e963d9
khushihappiness <74294754+khushihappiness@users.noreply.github.com>
2021-09-14 14:22:08 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,29 @@
+function setup()
+{
+    canvas=createCanvas(600, 500);
+    canvas.center();
+
+    video=createCapture(VIDEO);
+    video.hide();
+}
+
+song="";
+
+function preload()
+{
+    song=loadSound("music.mp3")
+}
+
+function draw()
+{
+    image(video, 0, 0, 600, 500);
+}
+function play()
+{
+    song.play();
+}
+
+function stop()
+{
+    song.stop();
+}
