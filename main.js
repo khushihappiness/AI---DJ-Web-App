44b2b3ca744b21cd0b6ff2c2db54791841561123
khushihappiness <74294754+khushihappiness@users.noreply.github.com>
2021-10-24 14:38:59 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -55,27 +55,27 @@ circle(rightWristX, rightWristY, 20);
 
 if(rightWristY > 0 && rightWristY <= 100)
      {
-document.getElementById("speed").innerHTML="Speed is = " + speed;
+document.getElementById("speed").innerHTML="Speed is = 0.5x ";
 song.rate(0.5);
      }
      else if(rightWristY > 100 && rightWristY <= 200)
      {
-document.getElementById("speed").innerHTML="Speed is = " + speed;
+document.getElementById("speed").innerHTML="Speed is = 1x";
 song.rate(1); 
      }
      else if(rightWristY > 200 && rightWristY <= 300)
      {
-document.getElementById("speed").innerHTML="Speed is = " + speed;
+document.getElementById("speed").innerHTML="Speed is = 1.5x";
 song.rate(1.5);   
      }
      else if(rightWristY > 300 && rightWristY <= 400)
      {
-document.getElementById("speed").innerHTML="Speed is = " + speed;
+document.getElementById("speed").innerHTML="Speed is = 2x";
 song.rate(2); 
      }
      else if(rightWristY > 400 && rightWristY <= 500)
      {
-document.getElementById("speed").innerHTML="Speed is = " + speed;
+document.getElementById("speed").innerHTML="Speed is = 2.5x";
 song.rate(2.5); 
      }
 
