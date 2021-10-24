108f0ea2b4fda658ccaf2f5a6fe45534c6cff8ec
khushihappiness <74294754+khushihappiness@users.noreply.github.com>
2021-10-24 14:33:03 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -23,6 +23,7 @@ leftWristY=0;
 rightWristX=0;
 rightWristY=0;
 scoreLeftWrist=0;
+scoreRightWrist=0;
 function preload()
 {
     song=loadSound("music.mp3")
@@ -44,6 +45,43 @@ function draw()
     document.getElementById("volume").innerHTML="volume" + volume;
     song.setVolume(volume);
 }
+
+if(scoreRightWrist > 0.2)
+ {
+
+fill('#FF0000');
+stroke("#FF0000");
+circle(rightWristX, rightWristY, 20);
+
+if(rightWristY > 0 && rightWristY <= 100)
+     {
+document.getElementById("speed").innerHTML="Speed is = " + speed;
+song.rate(0.5);
+     }
+     else if(rightWristY > 100 && rightWristY <= 200)
+     {
+document.getElementById("speed").innerHTML="Speed is = " + speed;
+song.rate(1); 
+     }
+     else if(rightWristY > 200 && rightWristY <= 300)
+     {
+document.getElementById("speed").innerHTML="Speed is = " + speed;
+song.rate(1.5);   
+     }
+     else if(rightWristY > 300 && rightWristY <= 400)
+     {
+document.getElementById("speed").innerHTML="Speed is = " + speed;
+song.rate(2); 
+     }
+     else if(rightWristY > 400 && rightWristY <= 500)
+     {
+document.getElementById("speed").innerHTML="Speed is = " + speed;
+song.rate(2.5); 
+     }
+
+    }
+
+
 }
 function play()
 {
@@ -63,7 +101,11 @@ function gotPoses(results)
     {
         console.log(results)
         scoreLeftWrist= results[0].pose.keypoints[9].score;
+        scoreRightWrist= results[0].pose.keypoints[10].score;
+
         console.log("Score is = " + scoreLeftWrist);
+        console.log("Score is = " + scoreRightWrist);
+
         leftWristX= results[0].pose.leftWrist.x;
         leftWristY= results[0].pose.leftWrist.y;
         console.log("Left wrist X = " + leftWristX + "Left wrist Y = " + leftWristY)
